33ecc1c550ef55d7e0ff5e1e9e3e65de98af9c53
Jackson Chen <sfc9982@googles.plus>
2024-11-08 20:42:44 +0800

    Tidy: Clean Code

--- grr.c
+++ grr.c
@@ -9,19 +9,17 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     int argc = 0;
-    LPWSTR* lpFont;
-    int countLoad = 0;
+    LPWSTR* lpFont = CommandLineToArgvW(GetCommandLine(), &argc);
+    int count_load = 0;
     wchar_t message[256];
-    int i = 1;
 
-    lpFont = CommandLineToArgvW(GetCommandLine(), &argc);
     if (argc < 2)
     {
         MessageBox(NULL, L"Drag and drop font file(s) onto .exe.", L"Usage", MB_OK | MB_ICONINFORMATION);
         return -1;
     }
 
-    for (i = 1; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
         if (AddFontResource(lpFont[i]) == 0)
         {
@@ -29,14 +27,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             goto removefont;
         }
         SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
-        ++countLoad;
+        ++count_load;
     }
     SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
-    wsprintfW(message, L"%d fonts loaded successfully.", countLoad);
+    wsprintfW(message, L"%d fonts loaded successfully.", count_load);
     MessageBox(NULL, L"Close this box to unload fonts.", message, MB_OK | MB_ICONINFORMATION);
 removefont:
     SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
-    for (i = 1; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
         RemoveFontResource(lpFont[i]);
         SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
